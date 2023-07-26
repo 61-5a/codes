520f86d3c10c3371f237218a65b274c7d39267b6
aZ <94156430+61-5a@users.noreply.github.com>
2023-07-26 21:34:50 +0530

    brew

---
+++ update-all.sh
@@ -0,0 +1,90 @@
+brew update && brew upgrade
+
+brew upgrade brotli
+brew upgrade ca-certificates
+brew upgrade curl
+brew upgrade ffmpeg
+brew upgrade openssl
+brew upgrade openssl@1.1
+brew upgrade perl
+brew upgrade postgresql
+brew upgrade postgresql@14
+brew upgrade powerlevel10k
+brew upgrade python
+brew upgrade python@3.10
+brew upgrade python@3.11
+brew upgrade sqlite
+
+brew upgrade docker
+brew upgrade geoip
+brew upgrade git
+brew upgrade htop
+brew upgrade jsmin
+brew upgrade mongodb/brew/mongodb-community
+brew upgrade nmap
+brew upgrade node
+brew upgrade php
+brew upgrade postgresql@14
+brew upgrade romkatv/powerlevel10k/powerlevel10k
+brew upgrade scrcpy
+brew upgrade tldr
+brew upgrade tmux
+brew upgrade tree
+brew upgrade vim
+brew upgrade watch
+brew upgrade wget
+brew upgrade wimlib
+brew upgrade zsh-autosuggestions
+
+brew upgrade --cask adobe-acrobat-reader
+brew upgrade --cask android-file-transfer
+brew upgrade --cask android-platform-tools
+brew upgrade --cask anydesk
+brew upgrade --cask balenaetcher
+brew upgrade --cask brave-browser
+brew upgrade --cask chromium
+brew upgrade --cask cloudflare-warp
+brew upgrade --cask cyberduck
+brew upgrade --cask discord
+brew upgrade --cask docker
+brew upgrade --cask dropbox
+brew upgrade --cask expressvpn
+brew upgrade --cask figma
+brew upgrade --cask firefox
+brew upgrade --cask forklift
+brew upgrade --cask gimp
+brew upgrade --cask google-chrome
+brew upgrade --cask google-drive
+brew upgrade --cask grammarly
+brew upgrade --cask grammarly-desktop
+brew upgrade --cask imageoptim
+brew upgrade --cask keepassxc
+brew upgrade --cask local
+brew upgrade --cask microsoft-edge
+brew upgrade --cask mongodb-compass
+brew upgrade --cask nitro-pdf-pro
+brew upgrade --cask openvpn-connect
+brew upgrade --cask opera
+brew upgrade --cask protonvpn
+brew upgrade --cask rar
+brew upgrade --cask signal
+brew upgrade --cask skype
+brew upgrade --cask sublime-text
+brew upgrade --cask tableplus
+brew upgrade --cask telegram
+brew upgrade --cask todoist
+brew upgrade --cask tor-browser
+brew upgrade --cask virtualbox
+brew upgrade --cask visual-studio-code
+brew upgrade --cask vivaldi
+brew upgrade --cask vlc
+# brew upgrade --cask vmware-fusion
+brew upgrade --cask vscodium
+brew upgrade --cask webstorm
+brew upgrade --cask whatsapp
+brew upgrade --cask wkhtmltopdf
+brew upgrade --cask zenmap
+brew upgrade --cask zoom
+
+brew autoremove && brew cleanup
+brew update && brew upgrade
---
+++ update.sh
@@ -0,0 +1,54 @@
+brew update && brew upgrade
+
+brew upgrade --cask adobe-acrobat-reader
+brew upgrade --cask android-file-transfer
+brew upgrade --cask android-platform-tools
+brew upgrade --cask anydesk
+brew upgrade --cask balenaetcher
+brew upgrade --cask brave-browser
+brew upgrade --cask chromium
+brew upgrade --cask cloudflare-warp
+brew upgrade --cask cyberduck
+brew upgrade --cask discord
+brew upgrade --cask docker
+brew upgrade --cask dropbox
+brew upgrade --cask expressvpn
+brew upgrade --cask figma
+brew upgrade --cask firefox
+brew upgrade --cask forklift
+brew upgrade --cask gimp
+brew upgrade --cask google-chrome
+brew upgrade --cask google-drive
+brew upgrade --cask grammarly
+brew upgrade --cask grammarly-desktop
+brew upgrade --cask imageoptim
+brew upgrade --cask keepassxc
+brew upgrade --cask local
+brew upgrade --cask microsoft-edge
+brew upgrade --cask mongodb-compass
+brew upgrade --cask nitro-pdf-pro
+brew upgrade --cask openvpn-connect
+brew upgrade --cask opera
+brew upgrade --cask protonvpn
+brew upgrade --cask rar
+brew upgrade --cask signal
+brew upgrade --cask skype
+brew upgrade --cask sublime-text
+brew upgrade --cask tableplus
+brew upgrade --cask telegram
+brew upgrade --cask todoist
+brew upgrade --cask tor-browser
+brew upgrade --cask virtualbox
+brew upgrade --cask visual-studio-code
+brew upgrade --cask vivaldi
+brew upgrade --cask vlc
+# brew upgrade --cask vmware-fusion
+brew upgrade --cask vscodium
+brew upgrade --cask webstorm
+brew upgrade --cask whatsapp
+brew upgrade --cask wkhtmltopdf
+brew upgrade --cask zenmap
+brew upgrade --cask zoom
+
+brew autoremove && brew cleanup
+brew update && brew upgrade
